d6f72332602c31ec072631fe1f2ee501fc604124
An106248 <An106248@donga.edu.vn>
2025-03-26 13:43:32 +0700

    buoi6

---
+++ product/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const rootRouter = require("./routes/root");
+const userRouter = require("./routes/user");
+const productRouter = require("./routes/product");
+
+const app = express();
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.use("/", rootRouter);
+app.use("/users", userRouter);
+app.use("/products", productRouter);
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(3000, () => {
+    console.log("Server Started!!!");
+});
---
+++ product/routes/product.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const ProductController = require("../controllers/product_controller");
+
+const router = express.Router();
+router.get("/", ProductController.index);
+
+module.exports = router;
